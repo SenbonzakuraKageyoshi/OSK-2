929fe8a5a2178fdf7ec45186c291161078122682
Mark <markwebdev@mail.ru>
2023-10-25 06:04:53 +0300

    Сделал переключение вопросов

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,5 +47,134 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     modalTriggers.forEach((el) => el.addEventListener('click', openModal));
-    closeModalButton.addEventListener('click', closeModal)
+    closeModalButton.addEventListener('click', closeModal);
+
+    const modalContent = document.querySelector('.modal-content-main'),
+    modalTitle = document.querySelector('.modal-content__header-title');
+
+    let step = 1;
+    console.log(step)
+    const answers = {
+        '1': {
+            question: 'Что собираетесь красить?',
+            answers: [
+                {name: 'Легковые автомобили', isSelected: true},
+                {name: 'Грузовые автомобили', isSelected: false},
+                {name: 'Металлоконструкции', isSelected: false},
+                {name: 'Другое  ', isSelected: false}
+            ]
+        },
+        '2': {
+            question: 'Как планируется установить окрасочную камеру?',
+            answers: [
+                {name: 'На ровный пол', isSelected: true},
+                {name: 'На приямок', isSelected: false},
+                {name: 'На металлическое основание', isSelected: false},
+            ]
+        },
+        '3': {
+            question: 'В каких условиях будет работать камера?',
+            answers: [
+                {name: 'Отапливаемое помещение', isSelected: true},
+                {name: 'Неотапливаемое помещение', isSelected: false},
+                {name: 'В условиях улицы', isSelected: false},
+            ]
+        },
+        '4': {
+            question: 'Какой вид топлива планируете использовать?',
+            answers: [
+                {name: 'Природный газ', isSelected: true},
+                {name: 'Электроэнергию', isSelected: false},
+                {name: 'Дизельное топливо', isSelected: false},
+            ]
+        }
+    };
+
+    const content = 
+    `
+    <div class="modal-content__question">${answers[`${step}`].question}</div>
+        <div class="modal-content-answers">
+            ${
+            answers[`${step}`].answers.map((el) => (
+                `
+                <div class="modal-content__answer ${el.isSelected && 'active'}">
+                    <div></div>
+                    ${el.name}
+                </div> 
+                `
+            ))
+            }
+        </div>
+        <div class="modal-content-nav">
+            ${
+            step !== 1
+            ?
+            `<button class="modal-content-nav__item decrement">Назад</button>`
+            :
+            `<button class="modal-content-nav__item disabled decrement" disabled>Назад</button>`
+            }
+            <button class="modal-content-nav__item increment">Вперед</button>
+        </div>
+    </div>
+    `
+
+    const increment = () => {
+        if(step !== 5){
+            step += 1
+            return initContent(step);
+        }
+    };
+
+    const decrement = () => {
+        if(step !== 1){
+            step -= 1
+            return initContent(step);
+        }
+    };
+
+    const initContent = (stepNumber) => {
+        if(stepNumber === 5){
+            modalContent.innerHTML =
+            `
+            `;
+
+            modalTitle.innerHTML = 'Мы обязательно перезвоним Вам в ближайший рабочий день'
+        }
+
+        modalContent.innerHTML =
+        `
+            <div class="modal-content__question">${answers[`${stepNumber}`].question}</div>
+                <div class="modal-content-answers">
+                    ${
+                    answers[`${stepNumber}`].answers.map((el) => (
+                        `
+                        <div class="modal-content__answer ${el.isSelected && 'active'}">
+                            <div></div>
+                            ${el.name}
+                        </div> 
+                        `
+                    ))
+                    }
+                </div>
+                <div class="modal-content-nav">
+                    ${
+                    stepNumber !== 1
+                    ?
+                    `<button class="modal-content-nav__item decrement">Назад</button>`
+                    :
+                    `<button class="modal-content-nav__item disabled decrement" disabled>Назад</button>`
+                    }
+                    <button class="modal-content-nav__item increment">Вперед</button>
+                </div>
+            </div>
+        `;
+
+
+        modalTitle.innerHTML = `Выберите оборудование, ответив на несколько простых вопросов (${stepNumber} / 4)`
+
+        document.querySelector('.increment').addEventListener('click', increment)
+        document.querySelector('.decrement').addEventListener('click', decrement)
+    };
+
+    initContent(step);
 });
